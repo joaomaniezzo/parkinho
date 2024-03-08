38eb41417992a6c1dd302a4b5fecbe3bf5a4f7d6
joaomaniezzo <joaodev2023@gmail.com>
2024-03-07 22:42:28 -0300

    fix form resubmission + span

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, json, redirect, url_for
 from flask_mysqldb import MySQL
 from datetime import datetime, timedelta, date
 
@@ -17,25 +17,30 @@ def home():
 
     return render_template('index.html')
 
-@app.route('/entrada', methods=['POST'])
+@app.route('/entrada', methods=['POST', 'GET'])
 def entrada():
 
     if request.method == 'POST':
-
         # _data = date.today().strftime("%d/%m/%Y") > apenas data formatada br
 
         _placa = request.form['inputPlacaEntrada']
         _modelo = request.form['inputModelo']
          #_horario = datetime.now().strftime("%H:%M:%S") > apenas horario formatado br
-        data = date.today()
-        horario_entrada = datetime.now()
+        if _placa and _modelo:
 
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO sua_tabela (data, placa, modelo, horario_entrada) VALUES (%s, %s,%s, %s)", (data, _placa, _modelo, horario_entrada))
+            data = date.today()
+            horario_entrada = datetime.now()
 
-        mysql.connection.commit()
+            cur = mysql.connection.cursor()
+            cur.execute("INSERT INTO sua_tabela (data, placa, modelo, horario_entrada) VALUES (%s, %s,%s, %s)", (data, _placa, _modelo, horario_entrada))
 
-        return render_template('index.html')
+            mysql.connection.commit()
+
+
+            return redirect(url_for('entrada'))
+    
+    return render_template('index.html')
+
 
 
 if __name__ == "__main__":
